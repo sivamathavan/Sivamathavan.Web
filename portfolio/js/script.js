b7e24bfc3c4ffe092df8f4975d134b46fa2acb88
Maddy <sivamathavan0212@gmail.com>
2025-03-03 01:09:53 +0530

    "C:\Users\Matha\Desktop\portfolio"
    This is my website

---
+++ portfolio/js/script.js
@@ -0,0 +1,8 @@
+//typing animation
+
+var typed =new Typed(".typing",{
+    strings:["","full stack developer","Java developer","Front-End Design","Back-End Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+});
